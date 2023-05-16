1f6e3bcf3d9f8b26fd5a4d90df1b6f8b2e50a296
unknown <ranxgarizaldi@gmail.com>
2023-05-16 15:43:49 +0700

    percobaan aplikasi

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Toggle class active
+const navbarNav = document.querySelector('.navbar-nav');
+
+// ketika hamburger menu diklik
+document.querySelector('#hamburger-menu').onclick = () => {
+  navbarNav.classList.toggle('active');
+};
+ // SOLVED CHATGPT 
+
+//klik diluar sidebar untuk menghilangkan nav
+const hamburger = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click', function (e) {
+ if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+  navbarNav.classList.remove('active');
+ }
+});
